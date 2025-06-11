c5ca138477c4da3837cf7bf56acfa3276ff29a07
Steven M. Caruso <51963719+smcaruso@users.noreply.github.com>
2025-06-11 17:36:30 -0400

    scripting progress

--- src/shareYourVision.ts
+++ src/shareYourVision.ts
@@ -11,6 +11,7 @@ export class ShareYourVision {
   themeOptions: HTMLDivElement[] = [document.createElement("div")]
   operatorChat: HTMLDivElement = document.createElement("div")
   operatorChatMessages: HTMLDivElement[] = []
+  messageHistory: HTMLDivElement = document.createElement("div")
   operatorChatInput: HTMLTextAreaElement = document.createElement("textarea")
   operatorChatSendButton: HTMLButtonElement = document.createElement("button")
 
@@ -110,15 +111,15 @@ export class ShareYourVision {
     this.themePicker.appendChild(themeOption)
 
     // Build operatorChat content using DOM
-    const messageHistory = document.createElement("div")
-    messageHistory.className = "message-history"
+    // const messageHistory = document.createElement("div")
+    this.messageHistory.className = "message-history"
 
     const userMessage = document.createElement("div")
     userMessage.className = "message user"
     userMessage.innerHTML = `
       <p>I want to build a virtual electronics store where people can browse and compare different products.</p>
     `
-    messageHistory.appendChild(userMessage)
+    this.messageHistory.appendChild(userMessage)
     this.operatorChatMessages.push(userMessage)
 
     const operatorMessage = document.createElement("div")
@@ -131,8 +132,10 @@ export class ShareYourVision {
       </div>
       <div class="progress-bar"></div>
     `
-    messageHistory.appendChild(operatorMessage)
+    this.messageHistory.appendChild(operatorMessage)
     this.operatorChatMessages.push(operatorMessage)
+
+    this.scrollMessageHistoryToBottom()
 
     const chatBox = document.createElement("div")
     chatBox.className = "chat-box"
@@ -146,6 +149,7 @@ export class ShareYourVision {
     const browseButton = document.createElement("button")
     browseButton.type = "button"
     browseButton.id = "browse-button"
+    browseButton.style.opacity = "0.5"
     browseButton.textContent = "Browse..."
 
     this.operatorChatSendButton = document.createElement("button")
@@ -157,7 +161,7 @@ export class ShareYourVision {
     chatBox.appendChild(browseButton)
     chatBox.appendChild(this.operatorChatSendButton)
 
-    this.operatorChat.appendChild(messageHistory)
+    this.operatorChat.appendChild(this.messageHistory)
     this.operatorChat.appendChild(chatBox)
 
     gsap.to(themeOption, {
@@ -166,12 +170,18 @@ export class ShareYourVision {
       duration: 0.5
     })
 
+    // build animations
+
     chatBox.style.opacity = "0"
+    chatBox.style.transform = "translateY(0.5rem)"
     operatorMessage.style.opacity = "0"
+    operatorMessage.style.transform = "translateY(0.5rem)"
     userMessage.style.opacity = "0"
+    userMessage.style.transform = "translateY(0.5rem)"
 
     gsap.to(chatBox, {
       opacity: 1,
+      transform: "unset",
       duration: 0.5,
       delay: 1,
       ease: "out"
@@ -179,17 +189,142 @@ export class ShareYourVision {
 
     gsap.to(userMessage, {
       opacity: 1,
+      transform: "unset",
       duration: 0.5,
       delay: 0,
       ease: "out"
     })
 
+    const operatorBuildMessages: string[] = [
+      "Matched “electronics” to 5 theme keywords.",
+      "Here's a theme we think you'll love: <strong>Guava Boutique</strong>.",
+      "See the list for some others that might be a good fit, too.",
+      "Do you have a <strong>website</strong> or <strong>Shopify store</strong> for your electronics store?"
+    ]
+
     gsap.to(operatorMessage, {
       opacity: 1,
+      transform: "unset",
       duration: 0.5,
       delay: 0.5,
-      ease: "out"
+      ease: "out",
+      onComplete: () => {
+        setTimeout(() => {
+          operatorMessage.innerHTML = `
+            <div class="message-header">
+              <img src="/ui/operator.svg" alt="Operator Icon" style="filter: brightness(0) invert(1);">
+              <span class="headline">${operatorBuildMessages[0]}</span>
+            </div>
+            <div class="divider" style="width:100%; border-bottom: 0.5px solid white;"> </div>
+          `
+
+          for (let i = 1; i <= 3; i++) {
+            setTimeout(() => {
+              const span = document.createElement("span")
+              span.className = "reasoning-message"
+              span.style.opacity = "0"
+              span.innerHTML = operatorBuildMessages[i]
+              operatorMessage.appendChild(span)
+              gsap.to(span, {opacity: 1, duration: 2})
+            }, i * 750)
+          }
+        }, 2000)
+      }
     })
+
+    const onSend = () => {
+      this.operatorChatInput.value = ""
+      this.operatorChatInput.style.height = 'auto'
+      this.operatorChatSendButton.removeEventListener('click', onSend)
+      this.suggestTheme()
+    }
+
+    this.operatorChatInput.addEventListener('input', () => {
+      this.operatorChatInput.value = "yeah, it's motherboard-metropolis.shopify.com"
+      this.operatorChatInput.style.height = 'auto' // reset to recalc
+      this.operatorChatInput.style.height = this.operatorChatInput.scrollHeight + 'px'
+      this.operatorChatSendButton.addEventListener( 'click', onSend.bind(this) )
+    })
+
+
+  }
+
+  suggestTheme() {
+
+    const userMessage = document.createElement("div")
+    userMessage.className = "message user"
+    userMessage.innerHTML = `
+      <p>yeah, it's motherboard-metropolis.shopify.com</p>
+    `
+    this.messageHistory.appendChild(userMessage)
+    this.operatorChatMessages.push(userMessage)
+
+    this.scrollMessageHistoryToBottom()
+
+    userMessage.style.opacity = "0"
+    userMessage.style.transform = "translateY(0.5rem)"
+
+    gsap.to(userMessage, {
+      opacity: 1,
+      transform: "unset",
+      duration: 0.5,
+      delay: 0.25,
+      ease: "out",
+      onComplete: () => {
+
+        const operatorMessage = document.createElement("div")
+        operatorMessage.className = "message operator"
+        operatorMessage.innerHTML = `
+          <p>Perfect. We can connect to your Shopify and import your products now.</p>
+          <regular-button>Connect Shopify</regular-button>
+        `
+
+        operatorMessage.style.opacity = "0"
+        operatorMessage.style.transform = "translateY(0.5rem)"
+
+        gsap.to(operatorMessage, {
+          opacity: 1,
+          transform: "unset",
+          duration: 0.5,
+          delay: 0.25,
+          ease: "out"
+        })
+
+        const connectShopifyButton = operatorMessage.querySelector("regular-button")
+        connectShopifyButton?.addEventListener("click", this.postShopifyDialog.bind(this))
+
+        this.messageHistory.appendChild(operatorMessage)
+
+        this.scrollMessageHistoryToBottom()
+
+      }
+    })
+
+  }
+
+  postShopifyDialog() {
+    const shopifyOverlay = document.createElement("div")
+    shopifyOverlay.classList.add("shopify-dialog")
+    this.main.append(shopifyOverlay)
+
+    shopifyOverlay.style.backdropFilter = "blur(0px)"
+    shopifyOverlay.style.backgroundColor = "#00000000"
+
+    gsap.to(shopifyOverlay, {
+      backgroundColor: "#00000022",
+      backdropFilter: "blur(12px)",
+      duration: 0.5,
+      ease: "out",
+      onComplete: () => {
+        shopifyOverlay.innerHTML = `<img src="/ui/shopify.png" alt="click to dismiss shopify dialog">`
+        shopifyOverlay.addEventListener("click", )
+      }
+    })
+
+  }
+
+  scrollMessageHistoryToBottom() {
+    this.messageHistory.scrollTop = this.messageHistory.scrollHeight
   }
 
 }
