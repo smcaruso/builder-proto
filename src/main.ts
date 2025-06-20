3ef4d8191c1858b2415a85860b000bc4f5ccb904
Steven M. Caruso <51963719+smcaruso@users.noreply.github.com>
2025-06-20 17:08:58 -0400

    Comment out ShareYourVision import in main.ts
    The import statement for ShareYourVision in main.ts has been commented out, possibly to temporarily disable its usage or due to it being unused.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css"
 import "./fonts.css"
 import { defineButtons } from "./components/buttons.ts"
 import { NavBar } from "./components/header.ts"
-import { ShareYourVision } from "./shareYourVision.ts"
+// import { ShareYourVision } from "./shareYourVision.ts"
 import { Viewport } from "./viewport.ts"
 import { Operator } from "./operator.ts"
 
